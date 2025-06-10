03e0598b1b451ae4f81e78844e270c5faa47e9a6
SiddiquiShifa <siddiquishifa007@gmail.com>
2025-06-10 23:35:58 +0530

    Update problemsolve.py

--- Dictionary/problemsolve.py
+++ Dictionary/problemsolve.py
@@ -1,17 +1,39 @@
 # sort  a dictionary by value
-a={"b":1,"c":2,"d":3,"e":4}
-sorted_dict=sorted(a.values())
-print(sorted_dict)
+adict={"b":6,"c":2,"d":3,"e":4}
+# sorted_dict=sorted(a.values())
+# print(sorted_dict)
 
 
-a={}
-for x in range(1,6):
-    key=int(input("Enter num : "))
-    value=key**2
-    a[key] = value                 # store in dictionary
-print(a)
+# a={}
+# for x in range(1,6):
+#     key=int(input("Enter num : "))
+#     value=key**2
+#     a[key] = value                 # store in dictionary
+# print(a)
     
     # value = input("Enter value: ") # u can take values a s input like this
 
-# print  a dict where keys are numbers between 1 to 15 values sqaure of keys
+# multiply all items in a dictionary
+b=1
+for x in adict:
+    b*=adict[x]
+    print(b)
 
+# # Check if a key exists
+# d={"name":"Shifa","age":21,"class":"TY"}
+# key="age"
+# if key in d:
+#     print(f"{key} exists with {d[key]}")
+# else:
+#     print("No")
+
+# Find the key with the maximum value
+adict={"b":6,"c":12,"d":3,"e":4}
+max_value=max(adict)
+print("Key with highes value is:",max_value)
+
+
+# to get the hoghest value 
+d = {"a": 10, "b": 30, "c": 20}
+max_value = max(d.values())
+print("Maximum value is:", max_value)
